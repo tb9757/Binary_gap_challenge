190742f165979dcf84ea22b3e174f97832bd2062
tb9757 <tb9757@gmail.com>
2026-01-07 16:50:20 +0000

    Add loop logic to count zeroes

--- binary_gap_calculator.py
+++ binary_gap_calculator.py
@@ -1,6 +1,15 @@
-A = bin(int(9))
-trimmed_A = A.lstrip('0b')
-print(trimmed_A)
+A = bin(int(37)).lstrip('0b')
 
-for i in range(len(trimmed_A)):
-    print(i)
+print(A)
+zero_counter = 0
+max_counter = 0
+started = False
+for i in range(len(A)):
+    if A[i] == '1':
+        if started:
+            max_counter = max(max_counter, zero_counter)
+        started = True
+        zero_counter = 0
+    elif A[i] == '0' and started:
+        zero_counter += 1
+print(max_counter)
